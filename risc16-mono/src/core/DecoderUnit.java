732cba10ef1cb24dc537c14a2ffaa9420d28366f
Eliandro <eliandro.gomes42@gmail.com>
2025-06-21 11:17:57 -0300

    implement DecoderUnit to decode raw instructions into Instruction objects

---
+++ risc16-mono/src/core/DecoderUnit.java
@@ -0,0 +1,27 @@
+package core;
+
+import instruction.Instruction;
+import util.BitUtils;
+
+public class DecoderUnit {
+    public Instruction decode(short raw) {
+        short format = BitUtils.extractBits(raw, 15, 1);
+        if(format == 0) {
+            return new Instruction(
+                    format,
+                    BitUtils.extractBits(raw, 9, 6),
+                    BitUtils.extractBits(raw, 6, 3),
+                    BitUtils.extractBits(raw, 3, 3),
+                    BitUtils.extractBits(raw, 0, 3)
+            );
+        } else {
+            return new Instruction(
+                    format,
+                    BitUtils.extractBits(raw, 13, 2),
+                    BitUtils.extractBits(raw, 10, 3),
+                    BitUtils.extractBits(raw, 0, 10),
+                    (short) 0
+            );
+        }
+    }
+}
