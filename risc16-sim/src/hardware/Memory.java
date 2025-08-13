82d7677e0883bf2741a14b4736068777f9dd38b2
Eliandro <eliandro.gomes42@gmail.com>
2025-08-12 22:27:48 -0300

    feat: adds toString method for memory report

--- risc16-sim/src/hardware/Memory.java
+++ risc16-sim/src/hardware/Memory.java
@@ -14,4 +14,19 @@ public class Memory {
     public void store(short adress, short value) {
         memory[adress] = value;
     }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("\n--- Memory Dump ---\n");
+
+        for (int i = 0; i < 15 && i < memory.length; i++) {
+            sb.append(String.format("[m%-3d]: %6d ", i, memory[i]));
+            if ((i + 1) % 4 == 0) {
+                sb.append("\n");
+            }
+        }
+
+        return sb.toString();
+    }
 }
